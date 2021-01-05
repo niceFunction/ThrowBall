ee702e6fe549b7cc15d8bca1baf05e47fe2324e7
Pussel <vincent.widegren@futuregames.nu>
2021-01-05 20:50:05 +0100

    UI + Some basic animations + Ball pickups and counter

---
+++ Assets/Code/Scripts/Pickup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private bool _isCollected;
+    public int pickupType = 1;
+    public SpriteRenderer theSR;
+    public Sprite blueSprite;
+    public Sprite redSprite;
+
+    void Start()
+    {
+        if (pickupType == 2)
+        {
+            theSR.sprite = blueSprite;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !_isCollected)
+        {
+            _isCollected = true;
+            Destroy(gameObject);
+            if ( pickupType == 1 ) { 
+                UIController.instance.PickUpRed();
+            }
+            if (pickupType == 2)
+            {
+                UIController.instance.PickUpBlue();
+            }
+        }
+    }
+}
---
+++ Assets/Code/Scripts/UIController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+
+    public float startTime = 500;
+
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI timer;
+    public TextMeshProUGUI blueCounter;
+    public TextMeshProUGUI yellowCounter;
+
+    public GameObject blueBall;
+    public GameObject redBall;
+    public GameObject noBall;
+    public GameObject FPSarms;
+
+    private Animation _anim;
+
+    private int _redBalls = 0;
+    private int _blueBalls = 0;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        UpdateScore(0);
+        UpdateBlueCounter(0);
+        UpdateYellowCounter(0);
+
+        blueBall.SetActive(false);
+        redBall.SetActive(false);
+        noBall.SetActive(true);
+
+        _anim = FPSarms.GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        startTime -= Time.deltaTime;
+        int intTimer = (int)startTime;
+        UpdateTimer(intTimer);
+
+        if (_blueBalls == 0 && _redBalls == 0)
+        {
+            PickUpNone();
+        }
+        
+    }
+
+    public void UpdateTimer(int newValue)
+    {
+        timer.SetText(newValue.ToString("000"));
+    }
+
+    public void UpdateScore(int newValue)
+    {
+        score.SetText(newValue.ToString("0000"));
+    }
+
+    public void UpdateBlueCounter(int newValue)
+    {
+        _blueBalls = _blueBalls + newValue;
+        blueCounter.SetText(newValue.ToString("0"));
+    }
+
+    public void UpdateYellowCounter(int newValue)
+    {
+        _redBalls = _redBalls + newValue;
+        yellowCounter.SetText(newValue.ToString("0"));
+    }
+
+    public void PickUpRed()
+    {
+        //_anim.Play("FPS_unequp");
+        blueBall.SetActive(false);
+        redBall.SetActive(true);
+        noBall.SetActive(false);
+
+        UpdateYellowCounter(1);
+    }
+
+    public void PickUpBlue()
+    {
+        //_anim.Play("FPS_unequp");
+        blueBall.SetActive(true);
+        redBall.SetActive(false);
+        noBall.SetActive(false);
+
+        UpdateBlueCounter(1);
+    }
+
+    public void PickUpNone()
+    {
+        //_anim.Play("FPS_unequp");
+        blueBall.SetActive(false);
+        redBall.SetActive(false);
+        noBall.SetActive(true);
+    }
+
+}

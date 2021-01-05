f9659d9e18d7b761ff389437b6ae97ae5210f247
Pussel <vincent.widegren@futuregames.nu>
2021-01-05 14:03:56 +0100

    Add billboard script + Crystal ball prefab

---
+++ Assets/Code/Scripts/Billboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera _theCam;
+
+    void Start()
+    {
+        _theCam = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(_theCam.transform);
+
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+    }
+}

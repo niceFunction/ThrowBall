947d47b61a4da1bcec935a0cbb7cdad33185b697
Pussel <vincent.widegren@futuregames.nu>
2021-01-06 13:01:00 +0100

    Added goals and almost made it work!

---
+++ Assets/Code/Scripts/GoalScore.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScore : MonoBehaviour
+{
+
+    public int scorePoints = 100;
+    public GameObject parentObject;
+    public float respawnInterval;
+    private float _respawnInterval;
+    private bool _scoredGoal;
+
+    void Start()
+    {
+        _respawnInterval = respawnInterval;
+    }
+
+    void Update()
+    {
+        if (_scoredGoal)
+        {
+            respawnInterval -= Time.deltaTime;
+            if (respawnInterval <= 0)
+            {
+                parentObject.SetActive(true);
+                _scoredGoal = false;
+            }
+            
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("RedBall") && !_scoredGoal)
+        {
+            _scoredGoal = true; // Scored a goal
+            respawnInterval = _respawnInterval; // Set interval timer to default
+            ScoreManager.Instance.AddToList(parentObject, respawnInterval);
+            parentObject.SetActive(false); // Inactivate parent
+        }/*
+        if (other.CompareTag("BlueBall") && !_scoredGoal)
+        {
+            _scoredGoal = true; // Scored a goal
+            respawnInterval = _respawnInterval; // Set interval timer to default
+            parentObject.SetActive(false); // Inactivate parent
+        }*/
+    }
+}
---
+++ Assets/Code/Scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+
+    public List<GameObject> goalObjects { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goalObjects = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddToList(GameObject goalObject, float interval)
+    {
+        goalObjects.Add(goalObject);
+    }
+
+}

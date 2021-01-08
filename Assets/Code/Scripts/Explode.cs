9ec015a1a54d513a3e155cdd4602f050be64bdce
Samuel Einheri <Samuel.einheri@gmail.com>
2021-01-08 10:55:26 +0100

    Added a explosion effect to the balls that "play" on throwable balls & pickupable balls destruction

---
+++ Assets/Code/Scripts/Explode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _explosionEffect;
+    [SerializeField, Range(1, 8)]
+    private float _destroyParticleTimer;
+
+    private void OnDestroy()
+    {
+        Instantiate(_explosionEffect, transform.position, Quaternion.identity);
+        Destroy(_explosionEffect, _destroyParticleTimer);
+    }
+}

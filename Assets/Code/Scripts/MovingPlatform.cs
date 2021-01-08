d3e597c90567c92249004a3c87323c6fba31ae6e
Pussel <vincent.widegren@futuregames.nu>
2021-01-08 11:04:19 +0100

    Updates to level begin to fix moving platforms

---
+++ Assets/Code/Scripts/MovingPlatform.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed;
+    public bool moveObj;
+    private bool _targetReached;
+    private bool _startReached;
+    private Vector3 _startPosition;
+
+    void Start()
+    {
+        target.parent = null;
+        _startPosition = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveObj == true && _targetReached == false)
+        {
+            float step = speed * Time.deltaTime; 
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+        if (moveObj == true && _targetReached == true)
+        {
+            float step = speed * Time.deltaTime; 
+            transform.position = Vector3.MoveTowards(transform.position, _startPosition, step);
+        }
+
+        if (!_targetReached)
+        {
+            if (Vector3.Distance(transform.position, target.position) > 0.01f)
+            {
+                // Moving
+            }
+            else
+            {
+                _startReached = false;
+                _targetReached = true;
+            }
+        }
+        if (_targetReached && !_startReached)
+        {
+            if (Vector3.Distance(transform.position, _startPosition) > 0.01f)
+            {
+                // Moving
+            }
+            else
+            {
+                _startReached = true;
+                _targetReached = false;
+            }
+        }
+        
+    }
+
+
+}
